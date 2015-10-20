754d3ddd355abad8d75ddb297e4e512645160f82
Conan Tzou <conan@asperasoft.com>
2015-10-20 14:45:39 -0700

    add directive functionalities

--- directives/functionalities.js
+++ directives/functionalities.js
@@ -1,17 +1,21 @@
 angular.module("functionalities", [])
   .directive("entering", function() {
     // shorthand if all we're going to do to the directive is use the linking function
-    return function(scope, element) {
+    return function(scope, element, attrs) {
       element.bind("mouseenter", function() {
         console.log("Mouse has entered the div");
+
+        element.addClass(attrs.entering);
       });
     };
   })
 
   .directive("leaving", function() {
-    return function(scope, element) {
+    return function(scope, element, attrs) {
       element.bind("mouseleave", function() {
         console.log("Mouse has left the div");
+
+        element.removeClass(attrs.entering);
       });
     };
   })
